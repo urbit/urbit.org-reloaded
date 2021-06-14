2e66872ce501e3c410ebdba8a4d25d65067fa1f6
Gavin Atkinson <gvnatknsn@protonmail.com>
2021-06-14 16:42:03 -0700

    first footer pass

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,52 @@
 import Link from 'next/link'
+import SingleColumn from './SingleColumn'
+import {contact} from '../lib/constants'
 
 export default function Footer() {
 	return (
-		<footer className="pt-12 bg-wall h-80 w-full mt-48">
-		  
+		<footer className="py-24 bg-wall w-full mt-48 flex justify-center">
+			<SingleColumn>
+				<div className="flex layout-wide">
+					<div className="w-1/3 flex flex-col">
+						<h3 className='mb-4'>Use Urbit</h3>
+						<Link className="mt-4" href=""><a className="type-ui text-gray">Urbit Client</a></Link>
+						<Link className="mt-4" href=""><a className="type-ui text-gray">Urbit Binaries</a></Link>
+						<Link className="mt-4" href=""><a className="type-ui text-gray">Urbit Apps</a></Link>
+						<Link className="mt-4" href=""><a className="type-ui text-gray">Host-Providers</a></Link>
+
+					</div>
+					<div className="w-1/3 flex flex-col">
+						<h3 className='mb-4'>About</h3>
+					</div>
+					<div className="w-1/3 flex flex-col">
+						<h3 className='mb-4'>News</h3>
+					</div>
+				</div>
+				<div className="flex layout-wide mt-24">
+					<div className="w-1/3 flex flex-col">
+						<h3 className='mb-4'>Develop</h3>
+						
+					</div>
+					<div className="w-1/3 flex flex-col">
+						<h3 className='mb-4'>Contribute</h3>
+					</div>
+					<div className="w-1/3 flex flex-col">
+						<h3 className='mb-4'>Community</h3>
+					</div>
+				</div>
+				<div className="flex layout-wide mt-64">
+					<div className="w-1/3">
+						<Link href=""><a className="type-ui text-gray">Privacy Policy</a></Link>
+						
+					</div>
+					<div className="w-1/3">
+						<Link href=""><a className="type-ui text-gray">Terms of Service</a></Link>
+					</div>
+					<div className="w-1/3">
+						<Link href=""><a className="type-ui text-gray">{contact.email}</a></Link>
+					</div>
+				</div>
+			</SingleColumn>
 		</footer>
 	)
 }
