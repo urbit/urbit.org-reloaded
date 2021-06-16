948799ef1b398a5c77f70f64d64bf3e047b02ae4
Gavin Atkinson <gvnatknsn@protonmail.com>
2021-06-15 20:18:16 -0700

    Add linter pre-commit hook, update readme

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,126 +1,141 @@
-import { useRouter } from 'next/router'
-import { getPostBySlug, getAllPosts, getNextPost, getPreviousPost, formatDate } from '../../lib/lib'
-import Head from 'next/head'
-import Link from 'next/link'
-import ErrorPage from '../404'
-import Container from '../../components/Container'
-import Markdown from '../../components/Markdown'
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
-import BackgroundImage from '../../components/BackgroundImage'
-import SingleColumn from '../../components/SingleColumn'
-
+import { useRouter } from "next/router";
 import {
-  name,
-} from '../../lib/constants'
+  getPostBySlug,
+  getAllPosts,
+  getNextPost,
+  getPreviousPost,
+  formatDate,
+} from "../../lib/lib";
+import Head from "next/head";
+import Link from "next/link";
+import ErrorPage from "../404";
+import Container from "../../components/Container";
+import Markdown from "../../components/Markdown";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import BackgroundImage from "../../components/BackgroundImage";
+import SingleColumn from "../../components/SingleColumn";
+
+import { name } from "../../lib/constants";
 
 export default function Post({ post, nextPost, previousPost }) {
-  const router = useRouter()
+  const router = useRouter();
   if (!router.isFallback && !post?.slug) {
-	return <ErrorPage />
+    return <ErrorPage />;
   }
-  
-  return (
-	<Container>
-		<SingleColumn>
-			<Header />
-			<section className="flex flex-col layout-wide pt-24">
-				<h1>{post.title}</h1>
-				
-				{
-					post.extra.author ? <div className='type-ui text-gray mt-4'>{post.extra.author}</div> : null
-				}
-				{
-					post.extra.ship ? <div className='type-ui text-gray font-mono'>{post.extra.ship}</div> : null
-				}
-				
-				<div className='type-ui text-gray mt-16'>{formatDate(new Date(post.date))}</div>
 
-			</section>
-			<article className="flex justify-center pt-12">
-		    	<Markdown post={post} />
-			</article>
-			<section className="layout-wide pt-24 flex">
-			
-			{
-				previousPost === null
-					? <div className={`w-1/2 mr-4`} />
-					: <div className={`cursor-pointer w-1/2 mr-4`} key={previousPost.slug}>
-					  <Link href={'/blog/' + previousPost.slug} key={`post-${previousPost.slug}`}>
-						<div>
-						<h3>Previous Post</h3>
-						  <BackgroundImage className="w-full h-80 rounded-lg" src={previousPost.extra.image || ''} />
-						  <p className='text-black mt-2'>{previousPost.title || ''}</p>
-						  <p className='text-gray'>{formatDate(new Date(previousPost.date))}</p>
-						</div>
-					  </Link>
-					</div>
-			}
-			{
-				nextPost === null
-					? <div className={`w-1/2 ml-4`} />
-					: <div className={`cursor-pointer w-1/2 ml-4`}>
-						  <Link href={'/blog/' + nextPost.slug} key={`post-${nextPost.slug}`}>
-							<div>
-								<h3>Next Post</h3>
-							  <BackgroundImage className="w-full h-80 rounded-lg" src={nextPost.extra.image || ''} />
-							  <p className='text-black mt-2'>{nextPost.title || ''}</p>
-							  <p className='text-gray'>{formatDate(new Date(nextPost.date))}</p>
-							</div>
-						  </Link>
-						</div>
-			}
-			</section>
-		</SingleColumn>
-		<Footer />
-	</Container>
-  )
+  return (
+    <Container>
+      <SingleColumn>
+        <Header />
+        <section className="flex flex-col layout-wide pt-24">
+          <h1>{post.title}</h1>
+          {post.extra.author ? (
+            <div className="type-ui text-gray mt-4">{post.extra.author}</div>
+          ) : null}
+          {post.extra.ship ? (
+            <div className="type-ui text-gray font-mono">{post.extra.ship}</div>
+          ) : null}
+          <div className="type-ui text-gray mt-16">
+            {formatDate(new Date(post.date))}
+          </div>
+        </section>
+        <article className="flex justify-center pt-12">
+          <Markdown post={post} />
+        </article>
+        <section className="layout-wide pt-24 flex">
+          {previousPost === null ? (
+            <div className={`w-1/2 mr-4`} />
+          ) : (
+            <div
+              className={`cursor-pointer w-1/2 mr-4`}
+              key={previousPost.slug}
+            >
+              <Link
+                href={"/blog/" + previousPost.slug}
+                key={`post-${previousPost.slug}`}
+              >
+                <div>
+                  <h3>Previous Post</h3>
+                  <BackgroundImage
+                    className="w-full h-80 rounded-lg"
+                    src={previousPost.extra.image || ""}
+                  />
+                  <p className="text-black mt-2">{previousPost.title || ""}</p>
+                  <p className="text-gray">
+                    {formatDate(new Date(previousPost.date))}
+                  </p>
+                </div>
+              </Link>
+            </div>
+          )}
+          {nextPost === null ? (
+            <div className={`w-1/2 ml-4`} />
+          ) : (
+            <div className={`cursor-pointer w-1/2 ml-4`}>
+              <Link
+                href={"/blog/" + nextPost.slug}
+                key={`post-${nextPost.slug}`}
+              >
+                <div>
+                  <h3>Next Post</h3>
+                  <BackgroundImage
+                    className="w-full h-80 rounded-lg"
+                    src={nextPost.extra.image || ""}
+                  />
+                  <p className="text-black mt-2">{nextPost.title || ""}</p>
+                  <p className="text-gray">
+                    {formatDate(new Date(nextPost.date))}
+                  </p>
+                </div>
+              </Link>
+            </div>
+          )}
+        </section>
+      </SingleColumn>
+      <Footer />
+    </Container>
+  );
 }
 
-// 
+//
 export async function getStaticProps({ params }) {
-	
-	const nextPost = getNextPost(params.slug, [
-		'title',
-		'slug',
-		'date',
-		'description',
-		'extra',
-	], 'blog') || null
-	
-	const previousPost = getPreviousPost(params.slug, [
-		'title',
-		'slug',
-		'date',
-		'description',
-		'extra',
-	], 'blog') || null
-				
-	const post = getPostBySlug(params.slug, [
-		'title',
-		'slug',
-		'date',
-		'description',
-		'content',
-		'extra',
-	], 'blog')
+  const nextPost =
+    getNextPost(
+      params.slug,
+      ["title", "slug", "date", "description", "extra"],
+      "blog"
+    ) || null;
 
-	return {
-		props: { post, nextPost, previousPost },
-	}
+  const previousPost =
+    getPreviousPost(
+      params.slug,
+      ["title", "slug", "date", "description", "extra"],
+      "blog"
+    ) || null;
+
+  const post = getPostBySlug(
+    params.slug,
+    ["title", "slug", "date", "description", "content", "extra"],
+    "blog"
+  );
+
+  return {
+    props: { post, nextPost, previousPost },
+  };
 }
 
 export async function getStaticPaths() {
-  const posts = getAllPosts(['slug', 'date'], 'blog')
+  const posts = getAllPosts(["slug", "date"], "blog");
 
   return {
-	paths: posts.map((post) => {
-	  return {
-		params: {
-		  slug: post.slug,
-		},
-	  }
-	}),
-	fallback: false,
-  }
+    paths: posts.map((post) => {
+      return {
+        params: {
+          slug: post.slug,
+        },
+      };
+    }),
+    fallback: false,
+  };
 }
--- pages/docs.js
+++ pages/docs.js
@@ -1,57 +1,57 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import { useState } from 'react';
-import Container from '../components/Container'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
-import SingleColumn from '../components/SingleColumn'
-import BackgroundImage from '../components/BackgroundImage'
-import Lorem from '../components/Lorem'
-import { getAllPosts, formatDate } from '../lib/lib'
+import Head from "next/head";
+import Link from "next/link";
+import { useState } from "react";
+import Container from "../components/Container";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import SingleColumn from "../components/SingleColumn";
+import BackgroundImage from "../components/BackgroundImage";
+import Lorem from "../components/Lorem";
+import { getAllPosts, formatDate } from "../lib/lib";
 
 function Sidebar(props) {
   return (
     <div className="flex flex-col w-72 bg-wall max-h-screen h-screen">
-      <header className='flex justify-between items-center pl-12 pt-8 pb-8'>
-        <Link href="/"><a className="type-ui">Urbit</a></Link>
+      <header className="flex justify-between items-center pl-12 pt-8 pb-8">
+        <Link href="/">
+          <a className="type-ui">Urbit</a>
+        </Link>
       </header>
       <div className="overflow-y-scroll p-12 pt-16">
-        { props.children }
-        <div className="pb-32"/>
+        {props.children}
+        <div className="pb-32" />
       </div>
     </div>
-  )
+  );
 }
 
 function ContentArea(props) {
   return (
     <div className="w-full">
-    <header className='flex justify-between items-center px-24 pt-8 pb-8'>
-      <div className="type-ui">Install Urbit</div>
-    </header>
-    <div className="px-24 pb-24 pt-16 flex flex-col w-full max-h-screen h-screen overflow-y-scroll">
-      <div className="type-ui text-lightGray">Breadcrumbs/go/here</div>
-      <h2 className="mb-16 mt-4">Getting Started</h2>
-      { props.children }
-      <div className="pb-32"/>
+      <header className="flex justify-between items-center px-24 pt-8 pb-8">
+        <div className="type-ui">Install Urbit</div>
+      </header>
+      <div className="px-24 pb-24 pt-16 flex flex-col w-full max-h-screen h-screen overflow-y-scroll">
+        <div className="type-ui text-lightGray">Breadcrumbs/go/here</div>
+        <h2 className="mb-16 mt-4">Getting Started</h2>
+        {props.children}
+        <div className="pb-32" />
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-
 export default function DocsLayout({ posts }) {
-    
   return (
-      <div className="flex w-screen h-screen min-h-screen w-screen overflow-hidden">
-        <Sidebar>
-          <Lorem/>
-        </Sidebar>
-        <ContentArea>
-            <Lorem/>
-        </ContentArea>
-      </div>
-  )
+    <div className="flex w-screen h-screen min-h-screen w-screen overflow-hidden">
+      <Sidebar>
+        <Lorem />
+      </Sidebar>
+      <ContentArea>
+        <Lorem />
+      </ContentArea>
+    </div>
+  );
 }
 
 // export async function getStaticProps() {
@@ -62,7 +62,7 @@ export default function DocsLayout({ posts }) {
 //     'description',
 //     'extra',
 //   ], 'docs')
-// 
+//
 //   return {
 //     props: { posts },
 //   }
