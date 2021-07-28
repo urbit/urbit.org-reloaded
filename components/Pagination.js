6493bd5926310a89ec6117229e08c6eb3bca3d51
Matilde Park <matilde@tlon.io>
2021-07-27 22:34:09 -0400

    pagination: truncate while preserving arrows

--- components/Pagination.js
+++ components/Pagination.js
@@ -7,12 +7,13 @@ export default function Pagination(props) {
       href={`/${props.section}/${props.post.slug}`}
       key={`post-${props.post.slug}`}
     >
-      <div className="bg-wall cursor-pointer font-semibold p-2 px-4 rounded-xl">
-        <p className="text-black">
-          {props.previous ? "<- " : ""}
-          {props.post.title}
-          {props.next ? " ->" : ""}
-        </p>
+      <div
+        className="bg-wall cursor-pointer font-semibold p-2 px-4 rounded-xl flex"
+        style={{ maxWidth: "45%" }}
+      >
+        <p className="flex-shrink-0">{props.previous ? "<- " : ""}</p>
+        <p className="text-black truncate">{props.post.title}</p>
+        <p className="flex-shrink-0">{props.next ? " ->" : ""}</p>
       </div>
     </Link>
   );
