01ea0d9a946843e6b29b91e097d8de2cc90f808b
Hunter Miller <hunter@thisdot.co>
2021-10-05 12:58:04 -0500

    markdown: fixing sanitization and upgrading prism that includes hoon

--- components/Markdown.js
+++ components/Markdown.js
@@ -5,6 +5,8 @@ import heading from "remark-heading-id";
 import html from "remark-html";
 import remarkprism from "remark-prism";
 import normalize from "mdurl/encode";
+import merge from "deepmerge";
+import github from "hast-util-sanitize/lib/github";
 
 // img is wrapped in figure so that images can be extra wide in the blog posts
 function Img(h, node) {
@@ -31,18 +33,23 @@ const options = {
     image: Img,
   },
   entities: "numbers",
+  sanitize: merge(github, {
+    // remove user-content from github.json to remark-slug work as expected
+    clobberPrefix: "",
+    attributes: { "*": ["className"] },
+  }),
 };
 
 // Converts markdown strings into markdown HTML/React components
 export default async function Markdown({ post }) {
   const result = await remark()
-    .use(html, options)
-    .use(gfm)
     .use(remarkprism, {
       plugins: ["show-invisibles"],
     })
-    .use(slug)
-    .use(heading)
+    .use(html, options)
+    //.use(gfm)
+    //.use(slug)
+    //.use(heading)
     .process(post.content);
 
   return result.toString();
