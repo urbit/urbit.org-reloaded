b69557d64ae865c9ee0d41d3774d61b0a4f38240
Hunter Miller <hunter@thisdot.co>
2021-10-06 11:22:57 -0500

    fixing markdown pipeline

--- components/Markdown.js
+++ components/Markdown.js
@@ -2,11 +2,14 @@ import remark from "remark";
 import gfm from "remark-gfm";
 import slug from "remark-slug";
 import heading from "remark-heading-id";
-import html from "remark-html";
 import remarkprism from "remark-prism";
 import normalize from "mdurl/encode";
 import merge from "deepmerge";
 import github from "hast-util-sanitize/lib/github";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import rehypeRaw from "rehype-raw";
 
 // img is wrapped in figure so that images can be extra wide in the blog posts
 function Img(h, node) {
@@ -32,7 +35,6 @@ const options = {
   handlers: {
     image: Img,
   },
-  entities: "numbers",
   sanitize: merge(github, {
     // remove user-content from github.json to remark-slug work as expected
     clobberPrefix: "",
@@ -43,13 +45,16 @@ const options = {
 // Converts markdown strings into markdown HTML/React components
 export default async function Markdown({ post }) {
   const result = await remark()
+    .use(remarkParse, options)
     .use(remarkprism, {
       plugins: ["show-invisibles"],
     })
-    .use(html, options)
-    //.use(gfm)
-    //.use(slug)
-    //.use(heading)
+    .use(gfm)
+    .use(slug)
+    .use(heading)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(rehypeStringify)
     .process(post.content);
 
   return result.toString();
