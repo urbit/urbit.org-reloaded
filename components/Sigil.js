6f2c2021fdfa3f6131d69d2ed1febf8f92adf772
Matilde Park <matilde.park@urbit.org>
2022-04-24 16:13:37 -0700

    sigil: enable icon sigils

--- components/Sigil.js
+++ components/Sigil.js
@@ -1,15 +1,16 @@
 import { sigil, reactRenderer } from "@tlon/sigil-js";
 
-export const Sigil = ({ patp, size }) => {
+export const Sigil = ({ patp, size, icon }) => {
   return (
-    <>
+    <div className={icon ? "p-1 bg-black" : ""}>
       {sigil({
         patp: patp,
         renderer: reactRenderer,
-        size: size,
+        size: icon ? size / 2 : size,
         colors: ["black", "white"],
+        icon: icon || false,
       })}
-    </>
+    </div>
   );
 };
 
