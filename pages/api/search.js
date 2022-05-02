458d7e17d73a97fac3a5edbd9f0b5fcd3a00f59b
Matilde Park <matilde.park@urbit.org>
2022-05-02 16:38:30 -0700

    search: fix case sensitivity in titles

--- pages/api/search.js
+++ pages/api/search.js
@@ -3,7 +3,7 @@ import { index } from "../../cache/data";
 export default (req, res) => {
   const results = index.filter(
     (e) =>
-      e.title?.toLowerCase().includes(req.query.q) ||
+      e.title?.toLowerCase().includes(req.query.q.toLowerCase()) ||
       e?.slug.includes(req.query.q)
   );
   res.statusCode = 200;
