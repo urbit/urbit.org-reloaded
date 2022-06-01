74ca72c14a8041abfcd0ce18905927984014fc2c
Matilde Park <matilde.park@urbit.org>
2022-06-01 15:53:05 -0700

    package.json: replace remark with markdoc

--- next.config.js
+++ next.config.js
@@ -1,47 +1,4 @@
-const withTM = require("next-transpile-modules")([
-  "remark-parse",
-  "remark-gfm",
-  "remark-rehype",
-  "rehype-stringify",
-  "rehype-raw",
-  "micromark-core-commonmark",
-  "micromark-extension-gfm",
-  "micromark-util-combine-extensions",
-  "micromark-util-chunked",
-  "micromark-util-character",
-  "micromark-util-sanitize-uri",
-  "micromark-util-encode",
-  "micromark-util-classify-character",
-  "micromark-util-resolve-all",
-  "micromark-util-subtokenize",
-  "micromark-util-normalize-identifier",
-  "micromark-util-html-tag-name",
-  "micromark-util-decode-numeric-character-reference",
-  "micromark-util-decode-string",
-  "micromark-factory-destination",
-  "micromark-factory-label",
-  "micromark-factory-space",
-  "micromark-factory-title",
-  "micromark-factory-whitespace",
-  "mdast-util-gfm",
-  "ccount",
-  "mdast-util-find-and-replace",
-  "hast-util-raw",
-  "hast-util-from-parse5",
-  "hast-util-to-parse5",
-  "hast-util-parse-selector",
-  "hast-to-hyperscript",
-  "hastscript",
-  "unist-util-visit-parents",
-  "unist-util-is",
-  "vfile-location",
-  "web-namespaces",
-  "mdast-util-to-markdown",
-  "mdast-util-from-markdown",
-  "markdown-table",
-]);
-
-module.exports = withTM({
+module.exports = {
   reactStrictMode: false,
   // target: 'serverless',
   webpack: (config, { isServer }) => {
@@ -66,4 +23,4 @@ module.exports = withTM({
     // }
     return config;
   },
-});
+};
