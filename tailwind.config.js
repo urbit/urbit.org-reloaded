46ea74faf3ff54ab7c8840ce699b3b9816c7ef48
Matilde Park <matilde.park@urbit.org>
2022-06-20 17:14:14 -0700

    Revert "Revert "package-lock: bump foundation-design-system""
    This reverts commit 391a59612212c939c08fff523a3becc1513ffaf5.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,3 @@
 module.exports = {
   presets: [require("foundation-design-system/tailwind.config")],
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./content/**/*.md",
-  ],
 };
