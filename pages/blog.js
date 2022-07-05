0aeb8c032851395bad09a1699961cd85b6a85c75
Matilde Park <matilde.park@urbit.org>
2022-07-05 11:30:42 -0700

    blog: remove pb from paragraph intro on md
    In two-column view it bleeds into the second column.

--- pages/blog.js
+++ pages/blog.js
@@ -31,7 +31,7 @@ export default function Blog({ posts, search }) {
         <Section>
           <h1 className="pb-16">Blog</h1>
           <div className="md:columns-2 gap-x-8">
-            <p className="pb-6">
+            <p className="pb-6 md:pb-0">
               Stories from the broader Urbit community, the Urbit Foundation,
               and the many people contributing to Urbit.
             </p>
@@ -45,49 +45,55 @@ export default function Blog({ posts, search }) {
           {posts.map((post) => {
             const date = generateDisplayDate(post.date);
             return (
-    <div key={post.slug} className="mb-20 cursor-pointer">
-      <Link href={`/blog/${post.slug}`}>
-        <div class="flex items-center md:flex-row flex-col">
+              <div key={post.slug} className="mb-20 cursor-pointer">
+                <Link href={`/blog/${post.slug}`}>
+                  <div class="flex items-center md:flex-row flex-col">
+                    <div class="flex w-full rounded-lg aspect-h-4 w-full">
+                      {
+                        // Not all blog posts have images
+                        post.extra.image ? (
+                          <BackgroundImage
+                            src={post.extra.image}
+                            className="rounded-lg aspect-w-5 aspect-h-4 w-full"
+                          />
+                        ) : (
+                          <div className="rounded-lg aspect-w-5 aspect-h-4 w-full bg-wall-100" />
+                        )
+                      }
+                    </div>
 
-          <div class="flex w-full rounded-lg aspect-h-4 w-full">
-          {
-            // Not all blog posts have images
-            post.extra.image ? (
-              <BackgroundImage
-                src={post.extra.image}
-                className="rounded-lg aspect-w-5 aspect-h-4 w-full"
-              />
-            ) : <div className="rounded-lg aspect-w-5 aspect-h-4 w-full bg-wall-100"/>
-          }
-          </div>
+                    <div class="w-full md:pl-6 ">
+                      <h3 className="mt-6 md:mt-0">{post.title}</h3>
+                      <div className="flex items-center justify-between mt-2">
+                        <div className="flex items-baseline">
+                          {post.extra.author ? (
+                            <div className="type-sub-bold mr-2">
+                              {post.extra.author}
+                            </div>
+                          ) : null}
+                          {post.extra.ship ? (
+                            <Link
+                              href={`https://urbit.org/ids/${post.extra.ship}`}
+                              passHref
+                            >
+                              <a className="type-sub-bold text-wall-500 font-mono">
+                                {post.extra.ship}
+                              </a>
+                            </Link>
+                          ) : null}
+                        </div>
+                      </div>
+                      <div className="text-wall-500 type-sub mt-1">
+                        {formatDate(date)}
+                      </div>
 
-          <div class="w-full md:pl-6 ">
-            <h3 className="mt-6 md:mt-0">{post.title}</h3>
-            <div className="flex items-center justify-between mt-2">
-              <div className="flex items-baseline">
-                {post.extra.author ? (
-                  <div className="type-sub-bold mr-2">{post.extra.author}</div>
-                ) : null}
-                {post.extra.ship ? (
-                  <Link
-                    href={`https://urbit.org/ids/${post.extra.ship}`}
-                    passHref
-                  >
-                    <a className="type-sub-bold text-wall-500 font-mono">
-                      {post.extra.ship}
-                    </a>
-                  </Link>
-                ) : null}
+                      {post?.description && (
+                        <p className="mt-6">{post.description}</p>
+                      )}
+                    </div>
+                  </div>
+                </Link>
               </div>
-            </div>
-            <div className="text-wall-500 type-sub mt-1">{formatDate(date)}</div>
-
-            {post?.description && <p className="mt-6">{post.description}</p>}
-
-          </div>
-        </div>
-      </Link>
-    </div>
             );
           })}
         </Section>
