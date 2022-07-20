13c55ac5f4353f7879be584f140f92398d5cc510
Matilde Park <matilde.park@urbit.org>
2022-07-19 18:25:12 -0700

    contact: update copy

--- components/Contact.js
+++ components/Contact.js
@@ -2,37 +2,23 @@ import NewsletterSignup from "./NewletterSignup";
 import { name, contact } from "../lib/constants";
 import Link from "next/link";
 
-export default function Contact({ emphasize }) {
+export default function Contact({
+  emphasize,
+  ecosystem = false,
+  className = "",
+}) {
   const linkText = emphasize ? "text-green-400" : "text-wall-600";
   const border = emphasize ? "border-green-400" : "border-wall-600";
 
   return (
-    <div>
+    <div className={className}>
       <div className="measure">
         <h4 className="pb-6">
-          If you’d like to follow our progress, we send monthly updates via
-          email:
+          Sign up for the Urbit Newsletter for a monthly Spotlight summary, and
+          more Urbit news.
         </h4>
       </div>
       <NewsletterSignup color={border} />
-      <h4 className="mt-12 text-wall-500">
-        Follow us on{" "}
-        <a className={linkText} href={contact.twitter}>
-          Twitter
-        </a>
-      </h4>
-      <h4 className="mt-6 text-wall-500">
-        Explore code on{" "}
-        <a className={linkText} href={contact.github}>
-          Github
-        </a>
-      </h4>
-      <h4 className="mt-6 text-wall-500 flex-wrap">
-        Boot Urbit and join{" "}
-        <Link href={`/groups/${contact.urbitCommunity}`} passHref>
-          <a className={linkText}>Urbit Community</a>
-        </Link>
-      </h4>
     </div>
   );
 }
