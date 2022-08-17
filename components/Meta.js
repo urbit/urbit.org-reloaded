8d9e23bb519b00dfa405d517f3d8d3f4df0dc55c
Matilde Park <matilde.park@urbit.org>
2022-08-17 11:47:33 -0700

    Meta: default to summary cards, not large_image

--- components/Meta.js
+++ components/Meta.js
@@ -1,16 +1,18 @@
-export default function Meta(post, disableImage) {
+export default function Meta(post, disableImage, large = false) {
   const author = post?.extra?.author || "Urbit";
   const title = post?.title ? `${post.title} - ` : "";
   const description =
     post?.description || "Urbit is a personal server built from scratch.";
   const image =
-    post?.extra?.image || post?.image || "https://media.urbit.org/logo/urbit-logo-card.png";
+    post?.extra?.image ||
+    post?.image ||
+    "https://media.urbit.org/logo/urbit-logo-card.png";
   return (
     <>
       <link rel="icon" type="image/png" href="/images/favicon.ico" />
       <meta
         name="twitter:card"
-        content="summary_large_image"
+        content={large ? "summary_large_image" : "summary"}
         key="twitter-card"
       />
       <meta name="twitter:site" content="@urbit" key="twitter-site" />
