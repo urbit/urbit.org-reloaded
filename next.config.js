3f7d143d2180107b9f531b01425aa68363df348f
Matilde Park <matilde.park@urbit.org>
2022-08-17 12:29:05 -0700

    package: add vercel-build

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
-// For exporting sigil pngs on Vercel, see https://github.com/Automattic/node-canvas/issues/1779#issuecomment-895885846
-if (
-  process.env.LD_LIBRARY_PATH == null ||
-  !process.env.LD_LIBRARY_PATH.includes(
-    `${process.env.PWD}/node_modules/canvas/build/Release:`
-  )
-) {
-  process.env.LD_LIBRARY_PATH = `${
-    process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
-}
-
 module.exports = {
   reactStrictMode: false,
   // target: 'serverless',
