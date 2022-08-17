47ddb6512156370404b0d5224126dbc013345752
Matilde Park <matilde.park@urbit.org>
2022-08-17 11:52:34 -0700

    Meta: tweak title

--- components/Meta.js
+++ components/Meta.js
@@ -1,5 +1,6 @@
 export default function Meta(post, disableImage, large = false) {
   const author = post?.extra?.author || "Urbit";
+  const title = post?.title ? post.title : "";
   const description =
     post?.description || "Urbit is a personal server built from scratch.";
   const image =
