5a024949a6b71d495f114b1289c7aebf3a7dabea
Matilde Park <matilde.park@urbit.org>
2022-08-17 11:52:54 -0700

    Meta: fallback to urbit.org author

--- components/Meta.js
+++ components/Meta.js
@@ -1,5 +1,5 @@
 export default function Meta(post, disableImage, large = false) {
-  const author = post?.extra?.author || "Urbit";
+  const author = post?.extra?.author || "urbit.org";
   const title = post?.title ? post.title : "";
   const description =
     post?.description || "Urbit is a personal server built from scratch.";
