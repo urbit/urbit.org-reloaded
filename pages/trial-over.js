1144faacaee3735b181fea842c4d05d20815e535
pkova <pyry.kovanen@gmail.com>
2022-09-23 12:06:39 +0300

    /trial: comet -> moon

--- pages/trial-over.js
+++ pages/trial-over.js
@@ -25,7 +25,7 @@ export default function TrialOver(props) {
       <IntraNav ourSite="https://urbit.org" search={props.search} />
       <div className="h-full w-full grow flex items-center justify-center flex-col px-8 md:px-0">
         <div className="w-fit">
-          <h2>Your one week comet trial has concluded.</h2>
+          <h2>Your one day moon trial has concluded.</h2>
           <p className="my-12 max-w-prose">To keep using Urbit, we recommend getting a planet and setting up Urbit yourself or using a hosting provider.
             <Link href="/getting-started"><a className="button-lg bg-green-400 text-white max-w-fit my-12">Get Started</a></Link></p>
         </div>
--- pages/trial.js
+++ pages/trial.js
@@ -37,7 +37,7 @@ const postReq = (path, params, method = 'post') => {
 export default function Trial(props) {
   const post = {
     title: "Explore the Network",
-    description: "Get one week of a complimentary hosted comet: a temporary Urbit ID."
+    description: "Get one day of a complimentary hosted moon: a temporary Urbit ID."
   };
 
   const [count, setCount] = useState(0)
@@ -81,7 +81,7 @@ export default function Trial(props) {
             {/* Blurb is flexed to space button and copy */}
             <div className="flex-col flex space-y-4">
               <p>No downloads, no signup.</p>
-              <p>Get <span className="font-bold">one week</span> of a complimentary hosted comet: a temporary Urbit ID.</p>
+              <p>Get <span className="font-bold">one day</span> of a complimentary hosted moon: a temporary Urbit ID.</p>
               {/* Button is flexed to space potential "try again later" copy */}
               <div className="xl:space-x-4 space-y-4 xl:space-y-0 self-center xl:self-start w-full flex-col xl:flex-row flex items-center">
                 <a className={cn("p-8 button-lg text-white max-w-fit shrink-0", {
@@ -111,14 +111,14 @@ export default function Trial(props) {
                       .catch(e => setCount(0));
                   }}
                 >
-                  {available ? 'Launch Comet' : 'No Comets Available'}
+                  {available ? 'Launch Moon' : 'No Moons Available'}
                 </a>
-                {!available && <p>Try back later!</p>}
+                {!available && <p>Check back later!</p>}
               </div>
             </div>
           </div>
           <div className="max-w-prose flex flex-col space-y-4">
-            <p>Comets are great for checking things out, but some groups may require a planet – a permanent Urbit ID.</p>
+            <p>Moons are great for checking things out, but some groups may require a planet – a permanent Urbit ID.</p>
             <p>Learn more about <Link href="/getting-started/get-planet"><a>getting a planet</a></Link>.</p>
           </div>
         </Section>
