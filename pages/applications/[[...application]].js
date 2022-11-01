bf45b69d7dd09aa860acc71cbeb9b2d2239f9f05
Matilde Park <matilde.park@urbit.org>
2022-11-01 14:01:26 -0700

    applications: add grant proposal if one exists under shortcode

--- pages/applications/[[...application]].js
+++ pages/applications/[[...application]].js
@@ -19,8 +19,9 @@ import Gateway404 from "../../components/gateway/Gateway404";
 import MetadataBlock from "../../components/gateway/MetadataBlock";
 import MetadataLink from "../../components/gateway/MetadataLink";
 import Description from "../../components/gateway/Description";
+import { getGrantByShortcode } from "../../lib/lib";
 
-const ApplicationPage = ({ data, markdown, organisation, search, params }) => {
+const ApplicationPage = ({ data, markdown, organisation, matchedGrant, search, params }) => {
   const { application } = params;
   if (!ob.isValidPatp(application[0])) {
     return <Gateway404 type="application" />;
@@ -83,7 +84,7 @@ const ApplicationPage = ({ data, markdown, organisation, search, params }) => {
             item="Application"
           />
 
-          <div className="flex flex-wrap md:flex-nowrap justify-between">
+          <div className="flex flex-wrap justify-between">
             <MetadataBlock
               title="License"
               content={data.license ? data.license : "Unknown"}
@@ -102,15 +103,19 @@ const ApplicationPage = ({ data, markdown, organisation, search, params }) => {
               className="basis-1/2 sm:basis-1/4"
               content={data.shortcode ? data.shortcode : data.title}
             />
+            {matchedGrant && <div className="basis-full flex flex-col space-y-1">
+              <p className="font-semibold text-wall-400">Grant Proposal</p>
+              <Link href={`/grants/${matchedGrant}`} passHref>
+                <a className="text-green-400 font-medium">urbit.org/grants/{matchedGrant}
+                </a>
+              </Link>
+            </div>}
           </div>
-
           <Description
             description={data.description}
             fallback="An application on Urbit."
             markdown={markdown}
           />
-
-          <hr className="text-white" />
 
           <div class="bg-wall-100 py-4 px-6 border-2 border-wall-200 rounded-xl">
             <p class="text-sm text-wall-400">
@@ -175,6 +180,7 @@ export const getServerSideProps = async ({ params, res }) => {
       ? JSON.stringify(Markdown.parse({ post: { content } }))
       : null;
 
+  const matchedGrant = getGrantByShortcode(params.application);
   if (!data.title) {
     data = {
       title: params.application?.join("/"),
@@ -187,6 +193,7 @@ export const getServerSideProps = async ({ params, res }) => {
       data,
       markdown,
       organisation,
+      matchedGrant,
       params,
     },
   };
