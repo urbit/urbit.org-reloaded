287975f64ee64fefe8be8585606a34c95b18cb07
Gordon <gordon@hey.com>
2022-11-22 16:22:50 -0800

    consolidating metadata

--- components/Meta.js
+++ components/Meta.js
@@ -10,6 +10,25 @@ export default function Meta(post, disableImage, large = false) {
   return (
     <>
       <link rel="icon" type="image/png" href="/images/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/images/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/images/site.webmanifest" />
+
       <meta
         name="twitter:card"
         content={large ? "summary_large_image" : "summary"}
