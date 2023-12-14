99393c446dd17d38713d3d18847e244a245201e2
ripa <22086553+ripperi@users.noreply.github.com>
2023-12-14 14:15:07 +0200

    Add dark/light mode switch

--- src/components/IntraNav.js
+++ src/components/IntraNav.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { IntraNav as IntraNavFDS } from "@urbit/fdn-design-system";
+import { DocSearch } from "@docsearch/react";
+import classnames from "classnames";
 
 const ourSite = {
   title: "Urbit.org",
@@ -26,13 +28,95 @@ const sites = [
 
 const pages = [];
 
-export default function IntraNav({ search }) {
+const prefersDark = () =>
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+const hasSetDark = () => {
+  const theme = localStorage.getItem("theme");
+  return !!theme && theme === "dark";
+};
+
+const respectSystemPreference = () => !localStorage.getItem("theme");
+
+export default function IntraNav({}) {
+  const [darkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    if (respectSystemPreference()) {
+      localStorage.removeItem("theme");
+      document.querySelector(":root").removeAttribute("theme");
+      if (prefersDark()) {
+        setDarkMode(true);
+      } else {
+        setDarkMode(false);
+      }
+    } else {
+      if (hasSetDark()) {
+        document.querySelector(":root").setAttribute("theme", "dark");
+        setDarkMode(true);
+      } else {
+        document.querySelector(":root").setAttribute("theme", "light");
+        setDarkMode(false);
+      }
+    }
+  });
+
+  const cycleTheme = () => {
+    if ((respectSystemPreference() && prefersDark()) || hasSetDark()) {
+      if (prefersDark()) {
+        localStorage.setItem("theme", "light");
+        document.querySelector(":root").setAttribute("theme", "light");
+      } else {
+        localStorage.removeItem("theme");
+        document.querySelector(":root").removeAttribute("theme");
+      }
+    } else {
+      if (!prefersDark()) {
+        localStorage.setItem("theme", "dark");
+        document.querySelector(":root").setAttribute("theme", "dark");
+      } else {
+        localStorage.removeItem("theme");
+        document.querySelector(":root").removeAttribute("theme");
+      }
+    }
+    setDarkMode(!darkMode);
+  };
+
+  const iconUrl = darkMode ? "/images/lightmode.svg" : "/images/darkmode.svg";
+
   return (
     <IntraNavFDS
       ourSite={ourSite}
       sites={sites}
       pages={pages}
-      search={search}
+      search={
+        <div className="flex h-full w-full justify-end">
+          <div
+            className={
+              "hidden xs:flex items-center h-full w-12 md:w-14 rounded-full bg-gray"
+            }
+          >
+            <button
+              className={classnames(
+                "flex items-center aspect-square h-6 md:h-8 mx-1 rounded-full bg-brite",
+                {
+                  "mr-auto": darkMode,
+                  "ml-auto": !darkMode,
+                }
+              )}
+              onClick={cycleTheme}
+            >
+              <div
+                className={"inline-block aspect-square w-3/4 m-auto bg-gray"}
+                style={{
+                  "-webkit-mask-image": `url(${iconUrl})`,
+                  "mask-image": `url(${iconUrl})`,
+                }}
+              />
+            </button>
+          </div>
+        </div>
+      }
     />
   );
 }
