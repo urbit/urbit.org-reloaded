1d12767054771c6d8f28e53665b61c3d885a49ca
ripa <22086553+ripperi@users.noreply.github.com>
2024-02-29 19:03:39 +0200

    Flip active and inactive link colors in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,12 @@ import classnames from "classnames";
 export default function Header({ pages = [], path }) {
   return (
     <div className="sticky top-12 md:top-16 z-40 layout">
-      <nav className="flex items-center w-full h-12 md:h-16 nav-space-x text-tertiary bg-surface whitespace-nowrap overflow-x-auto type-ui layout-px">
+      <nav className="flex items-center w-full h-12 md:h-16 nav-space-x text-primary bg-surface whitespace-nowrap overflow-x-auto type-ui layout-px">
         {pages.map((p) => (
           <Link
             className={classnames({
-              "text-primary": path === p.href,
-              "hover:text-primary": path !== p.href,
+              "text-tertiary": path === p.href,
+              "hover:text-tertiary": path !== p.href,
             })}
             href={p.href}
           >
