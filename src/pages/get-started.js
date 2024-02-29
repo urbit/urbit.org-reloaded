7aa0538659742b9ede489b4110a27c1c5f67790c
ripa <22086553+ripperi@users.noreply.github.com>
2024-02-29 18:50:15 +0200

    Replace holium with native planet on get started page

--- src/pages/get-started.js
+++ src/pages/get-started.js
@@ -15,7 +15,7 @@ const hosting = [
       "Blog post card: Square image full bleed to top, followed by copy snippet, Gray on Brite, 25px on 30px, Urbit Sans SemiBold (standard body copy properties).",
     extra: {
       image:
-        "https://storage.googleapis.com/media.urbit.org/blog/skybanner.jpg",
+        "https://storage.googleapis.com/media.urbit.org/site/hosting/red-horizon.png",
     },
     href: "https://redhorizon.com/",
   },
@@ -24,16 +24,20 @@ const hosting = [
     description:
       "Blog post card: Square image full bleed to top, followed by copy snippet, Gray on Brite, 25px on 30px, Urbit Sans SemiBold (standard body copy properties).",
     extra: {
-      image: "https://storage.googleapis.com/media.urbit.org/blog/b52.jpg",
+      image:
+        "https://storage.googleapis.com/media.urbit.org/site/hosting/tlon.png",
     },
     href: "https://tlon.io/",
   },
   {
-    title: "Holium",
+    title: "Self-hosting at home with Native Planet",
     description:
       "Some cards could feature solid colors, like this, which we will also be using for icon-based cards featured in guide pages. Another feature here is truncated text, eg this.",
-    extra: {},
-    href: "https://www.holium.com/",
+    extra: {
+      image:
+        "https://storage.googleapis.com/media.urbit.org/site/hosting/native-planet.png",
+    },
+    href: "https://www.nativeplanet.io/",
   },
 ];
 
@@ -52,7 +56,9 @@ export default function GetStarted({}) {
       <IntraNav />
       <Main className="text-primary" singleColumn>
         <section>
-          <h1 className="h1 mt-12 mb-8 md:mt-16 md:mb-16 lg:mb-20">Get Started</h1>
+          <h1 className="h1 mt-12 mb-8 md:mt-16 md:mb-16 lg:mb-20">
+            Get Started
+          </h1>
           <p className="h1">
             Urbit is a <strong>new kind of computer</strong> that you can own
             completely in ways that matter: <strong>networking</strong>,{" "}
@@ -75,35 +81,35 @@ export default function GetStarted({}) {
             ))}
           </FatBlock>
         </Section>
-        <Section divider={"border-primary"}>
-          <div>
-            <h2 className="h2">Self-hosting at home</h2>
-            <p className="body-lg">Running your Urbit is easier than ever</p>
-          </div>
-          <FatBlock className="flex bg-container-variant text-on-container rounded-lg p-4">
-            <img
-              className="aspect-square w-1/3 object-cover mr-4 xs:mr-8 md:mr-16"
-              alt=""
-              src="https://s3-alpha-sig.figma.com/img/d233/6719/90cc0290296014c2898a31013706c329?Expires=1705881600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MU4ixgomkq8CurHpdVnNtmMHITXhiUtU1xAAsVnAhW-cD1Byqq7HgVoC6f8FC3~kEFl6J7MJ86yck1R-iLdzJJcwijcLp6u5-a8RUnKbaJwpa8KobafInX3cneaeM0mdN3Dpx1F8tpmOqX0xvhoHVMYvzRFtcJzlqADP~2vDv9oh9MO2KNLeYZHS~22ylINIBdzNo2g1hMmDMoD6X4AjpafgmNnXcd~C6a57jz-uTAE3ANJ0f9IgGUa80IO7IDpXfzimqc6TozzK54Jb8CDD5o5zRBdpYiJq89UL-atcycn5CxXpHzXmWeAqckZsDKaNY6gLvXIr8d0AFqc262JfYA__"
-            />
-            <div className="flex flex-col justify-between">
-              <h3 className="hidden xs:block h1">Native Planet</h3>
-              <h3 className="xs:hidden h2">Native Planet</h3>
-              <div className="body-md">
-                <p className="mb-3.5">
-                  Native Planet copy goes here, about how easy it is to set up
-                  your own Native Planet
-                </p>
-                <Link
-                  className="btn bg-on-container text-inverse-on-container hover:bg-on-container-variant"
-                  href="https://www.nativeplanet.io/"
-                >
-                  Find out more
-                </Link>
-              </div>
-            </div>
-          </FatBlock>
-        </Section>
+        {/* <Section divider={"border-primary"}> */}
+        {/*   <div> */}
+        {/*     <h2 className="h2">Self-hosting at home</h2> */}
+        {/*     <p className="body-lg">Running your Urbit is easier than ever</p> */}
+        {/*   </div> */}
+        {/*   <FatBlock className="flex bg-container-variant text-on-container rounded-lg p-4"> */}
+        {/*     <img */}
+        {/*       className="aspect-square w-1/3 object-cover mr-4 xs:mr-8 md:mr-16" */}
+        {/*       alt="" */}
+        {/*       src="https://s3-alpha-sig.figma.com/img/d233/6719/90cc0290296014c2898a31013706c329?Expires=1705881600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MU4ixgomkq8CurHpdVnNtmMHITXhiUtU1xAAsVnAhW-cD1Byqq7HgVoC6f8FC3~kEFl6J7MJ86yck1R-iLdzJJcwijcLp6u5-a8RUnKbaJwpa8KobafInX3cneaeM0mdN3Dpx1F8tpmOqX0xvhoHVMYvzRFtcJzlqADP~2vDv9oh9MO2KNLeYZHS~22ylINIBdzNo2g1hMmDMoD6X4AjpafgmNnXcd~C6a57jz-uTAE3ANJ0f9IgGUa80IO7IDpXfzimqc6TozzK54Jb8CDD5o5zRBdpYiJq89UL-atcycn5CxXpHzXmWeAqckZsDKaNY6gLvXIr8d0AFqc262JfYA__" */}
+        {/*     /> */}
+        {/*     <div className="flex flex-col justify-between"> */}
+        {/*       <h3 className="hidden xs:block h1">Native Planet</h3> */}
+        {/*       <h3 className="xs:hidden h2">Native Planet</h3> */}
+        {/*       <div className="body-md"> */}
+        {/*         <p className="mb-3.5"> */}
+        {/*           Native Planet copy goes here, about how easy it is to set up */}
+        {/*           your own Native Planet */}
+        {/*         </p> */}
+        {/*         <Link */}
+        {/*           className="btn bg-on-container text-inverse-on-container hover:bg-on-container-variant" */}
+        {/*           href="https://www.nativeplanet.io/" */}
+        {/*         > */}
+        {/*           Find out more */}
+        {/*         </Link> */}
+        {/*       </div> */}
+        {/*     </div> */}
+        {/*   </FatBlock> */}
+        {/* </Section> */}
         <Section tight divider={"border-primary"}>
           <h2 className="h1">Power user guides</h2>
           <p className="body-lg">
