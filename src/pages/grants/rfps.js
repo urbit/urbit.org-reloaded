6849316550b125939f62ed45e9f66d81d61de3ad
ripa <22086553+ripperi@users.noreply.github.com>
2024-03-01 17:53:59 +0200

    Add Requests for Proposals page

---
+++ src/pages/grants/rfps.js
@@ -0,0 +1,29 @@
+import React from "react";
+import GrantProgramPage from "@/components/GrantProgramPage";
+import { Markdown, getPostBySlug } from "@urbit/fdn-design-system";
+
+export default function rfps({ post, markdown }) {
+  return (
+    <GrantProgramPage
+      program="Requests for Proposals"
+      post={post}
+      markdown={markdown}
+      actionText="View RFPs"
+      actionLink="/grants?program=RFP#view-grants"
+    />
+  );
+}
+
+export async function getStaticProps() {
+  const post = getPostBySlug(
+    "/rfps",
+    ["title", "date", "slug", "content"],
+    "/"
+  );
+
+  const markdown = JSON.stringify(Markdown.parse({ post }));
+
+  return {
+    props: { post, markdown },
+  };
+}
