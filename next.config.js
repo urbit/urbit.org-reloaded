694ef3430a1bbb46a5d562cbe94a1a7432269a94
pkova <pyry@urbit.org>
2024-03-06 11:03:49 +0200

    Fix broken urbit.org install links

--- next.config.js
+++ next.config.js
@@ -1,5 +1,29 @@
 module.exports = {
   reactStrictMode: false,
+  async redirects() {
+    return [
+    {
+      "source": "/install/linux-aarch64/latest",
+      "destination": "https://github.com/urbit/vere/releases/latest/download/linux-aarch64.tgz",
+      "permanent": true
+    },
+    {
+      "source": "/install/linux-x86_64/latest",
+      "destination": "https://github.com/urbit/vere/releases/latest/download/linux-x86_64.tgz",
+      "permanent": true
+    },
+    {
+      "source": "/install/macos-aarch64/latest",
+      "destination": "https://github.com/urbit/vere/releases/latest/download/macos-aarch64.tgz",
+      "permanent": true
+    },
+    {
+      "source": "/install/macos-x86_64/latest",
+      "destination": "https://github.com/urbit/vere/releases/latest/download/macos-x86_64.tgz",
+      "permanent": true
+    },
+    ];
+  },
   // target: 'serverless',
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
