0a290a937530198ef7b255b469a9c877e72af111
Remy Hunter <mail@remyhunter.work>
2024-09-24 13:17:56 -0400

    update container on grants posts

--- app/grants/[blog]/page.js
+++ app/grants/[blog]/page.js
@@ -3,44 +3,48 @@ import { formatDate, formatAuthors } from "../../lib/utils";
 import Markdoc from "@markdoc/markdoc";
 import React from "react";
 import { glob } from "glob";
-import path from 'path';
+import path from "path";
 import classNames from "classnames";
-
 
 const BLOG_PATH = "app/docs/grants";
 const POSTS_DIR = path.join(process.cwd(), BLOG_PATH);
 
 export async function generateStaticParams() {
-  const postPaths = await glob(path.join(POSTS_DIR, '**/*.md'));
-  const paths = postPaths?.map(postPath => {
-    return { 
-      blog: path.basename(postPath, '.md') // Strip the .md extension
+  const postPaths = await glob(path.join(POSTS_DIR, "**/*.md"));
+  const paths = postPaths?.map((postPath) => {
+    return {
+      blog: path.basename(postPath, ".md"), // Strip the .md extension
     };
   });
 
   return paths;
 }
 
-
 export default async function PostPage({ params }) {
   const postSlug = `/grants/${params.blog}.md`; // Append .md here to use in the file path
   const postData = await getMarkdownContent(postSlug);
   const postMeta = await getYaml(postSlug);
-  const config = await getYaml('/config.md');
+  const config = await getYaml("/config.md");
 
   const components = postData.components;
 
   return (
-    <section className="container">
-      <div>
-        <span className="">{postData.frontMatter.title}</span>
-        <div className="">
-          <span className="">{formatDate(postData.frontMatter.date)}</span>
-          <span className="">{formatAuthors(postData.frontMatter.authors)}</span>
+    <section className="grid grid-cols-6 mb-32 container">
+      <div className="col-span-1"></div>
+      <div className="col-span-5">
+        <div>
+          {" "}
+          <span className="">{postData.frontMatter.title}</span>
+          <div className="">
+            <span className="">{formatDate(postData.frontMatter.date)}</span>
+            <span className="">
+              {formatAuthors(postData.frontMatter.authors)}
+            </span>
+          </div>
         </div>
-      </div>
-      <div className="">
-        {Markdoc.renderers.react(postData.content, React, { components })}
+        <div className="">
+          {Markdoc.renderers.react(postData.content, React, { components })}
+        </div>
       </div>
     </section>
   );
