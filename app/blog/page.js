2274ed3ab4f4686f7186512cc95da889193032e0
Remy Hunter <mail@remyhunter.work>
2024-10-30 13:56:22 -0400

    clean up comments

--- app/blog/page.js
+++ app/blog/page.js
@@ -6,17 +6,13 @@ export default async function BlogHome() {
 
   const allPostFrontMatter = [];
   await Promise.all(posts.map(async (post) => {
-    // console.log(post.relativePath)
     const frontmatter = await getToml(post.relativePath);
-    // console.log(frontmatter);
     allPostFrontMatter.push({
       data: frontmatter.data,
       relativePath: post.relativePath,
       slug: post.slug
     });
   }));
-
-  console.log('got here', allPostFrontMatter);
 
   return (
     <div className="container mb-32 md:mt-[3.06rem]">
