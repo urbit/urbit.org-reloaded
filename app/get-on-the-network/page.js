d7c3da8d53c3947c080b930d5c4537142f5a3dfd
Remy Hunter <mail@remyhunter.work>
2024-10-30 13:58:53 -0400

    add get on the network placeholder

---
+++ app/get-on-the-network/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { getMarkdownContent } from "../lib/queries";
+import Link from "next/link";
+
+import Markdoc from "@markdoc/markdoc";
+
+export default async function overview() {
+
+  return (
+    <div className="container mb-[8rem] md:mb-[13.4rem] mt-12 overview-page">
+      get on the network placeholder
+    </div>
+  );
+}
