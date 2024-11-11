ef3e30181594bac280d8fca85517f58ab394237b
Remy Hunter <mail@remyhunter.work>
2024-11-11 15:34:49 -0500

    update mobile layout for blog roll

--- app/blog/page.js
+++ app/blog/page.js
@@ -3,6 +3,8 @@ import { getPostsTree, getToml } from "../lib/queries";
 import Image from "next/image";
 import Link from "next/link";
 
+import { formatDate } from "../lib/utils";
+
 export default async function BlogHome() {
   const posts = await getPostsTree("blog/");
 
@@ -37,14 +39,19 @@ export default async function BlogHome() {
             key={post.slug}
             className="block md:grid group  grid-cols-6 gap-x-4 w-full my-16 "
           >
-            <div className="col-span-1 flex flex-col mb-4 transition-all font-mono !text-large tracking-[.01em] text-gray-f5">
+            <div className="col-span-1 hidden md:flex flex-col mb-4 transition-all font-mono !text-large tracking-[.01em] text-gray-f5">
               <div className="mb-[.1em] ">{extra.author}</div>
               <div>{extra.ship}</div>
             </div>
-            <div className=" col-span-4 group-hover:!text-gray-87 transition-all  flex flex-col leading-[120%]">
+            <div className="col-span-4 group-hover:!text-gray-87 transition-all  flex flex-col leading-[120%]">
               <div className="font-bold">{title}</div>
-              <div>{description}</div>
-              <div className="text-gray-87  mb-4">{date}</div>
+              <div className="mb-4 md:mb-0">{description}</div>
+              
+              <div className="flex md:hidden flex-row justify-between">
+                <div className="">{extra.author}</div>
+                <div>{extra.ship}</div>
+              </div>
+              <div className="text-gray-87 mb-8">{formatDate(date)}</div>
               <div className="h-[300px] w-auto relative">
                 <img className="h-full w-auto" loading="lazy" src={extra.image} alt={title} />
               </div>
