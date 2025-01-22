3eeddd428b2c55963b20640dd140d92b4f185a46
Remy Hunter <mail@remyhunter.work>
2025-01-22 13:50:47 -0500

    update font sizes and footer layout

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default async function HomePage() {
   return (
     <div className="container relative z-[-100] top-0 w-full h-full mb-[4.875rem] !mt-[0px]">
       {/* <SigilCard /> */}
-      <h1 className="text-6xl flex flex-col leading-[110%] tracking-[.03em] font-[300]">
+      <h1 className="text-2xlarge lg:text-3xlarge 2xl:text-4xlarge  flex flex-col leading-[110%] tracking-[.03em] font-[300]">
       <span className="whitespace-nowrap">A change in strategy, the Urbit project is rebooting.</span>
       <span className="whitespace-nowrap">Our vision hasn’t changed, but how we get there has.</span>
       <span className="whitespace-nowrap text-gray-87">What’s coming is worth the wait. </span>
