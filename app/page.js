28823940f27acfaa5fc5111dabd478603de3fa89
Remy Hunter <mail@remyhunter.work>
2025-01-22 11:33:54 -0500

    update header nav font sizing

--- app/page.js
+++ app/page.js
@@ -6,8 +6,14 @@ export default async function HomePage() {
   // const homepage = await getMarkdownContent("index.md")
 
   return (
-    <div className="container relative z-[-100] top-0 w-full h-full !mt-[0px]">
-      <SigilCard />
+    <div className="container relative z-[-100] top-0 w-full h-full mb-[4.875rem] !mt-[0px]">
+      {/* <SigilCard /> */}
+      <h1 className="text-6xl flex flex-col leading-[110%] tracking-[.03em] font-[300]">
+      <span className="whitespace-nowrap">A change in strategy, the Urbit project is rebooting.</span>
+      <span className="whitespace-nowrap">Our vision hasn’t changed, but how we get there has.</span>
+      <span className="whitespace-nowrap text-gray-87">What’s coming is worth the wait. </span>
+      <span className="whitespace-nowrap text-gray-87">Sign up below to be the first to know.</span>
+      </h1>
     </div>
   );
 }
