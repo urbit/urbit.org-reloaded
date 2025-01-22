595bb0fc3f90e78419e6ead07a3bc3e2545e2501
Remy Hunter <mail@remyhunter.work>
2025-01-22 16:23:43 -0500

    fix block layout of homepage messaging

--- app/page.js
+++ app/page.js
@@ -8,11 +8,15 @@ export default async function HomePage() {
   return (
     <div className="container relative z-[-100] top-0 w-full h-full mb-[4.875rem] !mt-[0px]">
       {/* <SigilCard /> */}
-      <h1 className="text-size-homepage flex flex-col leading-[110%] tracking-[.03em] font-[300]">
-      <span className="whitespace-nowrap">A change in strategy, the Urbit project is rebooting.</span>
-      <span className="whitespace-nowrap">Our vision hasn’t changed, but how we get there has.</span>
-      <span className="whitespace-nowrap text-gray-87">What’s coming is worth the wait. </span>
-      <span className="whitespace-nowrap text-gray-87">Sign up below to be the first to know.</span>
+      <h1 className="text-size-homepage inline-block leading-[110%] tracking-[.03em] font-[300]">
+      <span className="">A change in strategy, the Urbit project is rebooting.</span>
+      <br/>
+      <span className="">Our vision hasn’t changed, but how we get there has.</span>
+      <br/>
+      <span className="text-gray-87">What’s coming is worth the wait. </span>
+      <br/>
+      <span className="text-gray-87">Sign up below to be the first to know.</span>
+      <br/>
       </h1>
     </div>
   );
