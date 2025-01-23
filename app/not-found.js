0c86e47a1d683471b879820f850697ae5b1ed9e4
Remy Hunter <mail@remyhunter.work>
2025-01-23 11:47:01 -0500

    update not found

--- app/not-found.js
+++ app/not-found.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function NotFound() {
   return (
     <div className="min-h-[84svh] flex flex-col items-center justify-center">
-      <h2>404 Not Found 🔦</h2>
+      <h2>Page Not Found</h2>
       <Link href="/">Return Home</Link>
     </div>
   )
