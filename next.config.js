d91bb971ec3f42a1ff41c91cf0c0bd0e3c8d1e8e
master-malwyl <93844417+master-malwyl@users.noreply.github.com>
2025-02-02 17:25:05 +0400

    restore binary download redirects

--- next.config.js
+++ next.config.js
@@ -8,8 +8,32 @@ const nextConfig = {
     unoptimized: true,
   },
   darkMode: 'class',
-  output: 'export',
+  // removed output: 'export'
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdoc'],
+  async redirects() {
+    return [
+      {
+        source: "/install/linux-aarch64/latest",
+        destination: "https://github.com/urbit/vere/releases/latest/download/linux-aarch64.tgz",
+        permanent: true
+      },
+      {
+        source: "/install/linux-x86_64/latest",
+        destination: "https://github.com/urbit/vere/releases/latest/download/linux-x86_64.tgz",
+        permanent: true
+      },
+      {
+        source: "/install/macos-aarch64/latest",
+        destination: "https://github.com/urbit/vere/releases/latest/download/macos-aarch64.tgz",
+        permanent: true
+      },
+      {
+        source: "/install/macos-x86_64/latest",
+        destination: "https://github.com/urbit/vere/releases/latest/download/macos-x86_64.tgz",
+        permanent: true
+      },
+    ];
+  },
 }
 
 const markdocConfig = {
@@ -17,4 +41,3 @@ const markdocConfig = {
 }
 
 module.exports = withMarkdoc(markdocConfig)(nextConfig);
- 
